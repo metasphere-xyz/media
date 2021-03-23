850e0b67fabe8cf14ac43796fa70174ee5b8160a
Julian Fleck <mail@julianfleck.net>
2021-03-23 18:26:10 +0100

    added configuration file

---
+++ processors/configuration/ecchr.config.py
@@ -0,0 +1,24 @@
+# Configuration file for metasphere processor
+
+# scope: episode IDs
+
+episodes = {
+    '4ece0d064575609367f665d617d95d30':
+        'Visuals of Violence with Mark Sealy and Wolfgang Kaleck',
+    '7cd79ca6d4999a07e969b647a82057eb':
+        'On artistic and human rights interventions 1/4 with Fred Ritchin and Wolfgang Kaleck',
+    '275b1e9261bbfb863c0e62cfa2aa9d73':
+        'Visuals of Violence with Mark Sealy and Wolfgang Kaleck',
+    '445b28f26e7c2d54a6cbc6deeb57bc49':
+        'Ixmucané Aguilar and Wolfgang Kaleck'
+}
+
+# Entity matching
+
+matched_entities = {
+    'Mark': 'Mark Sealy',
+    'Wolfgang': 'Wolfgang Kaleck',
+    'Fred': 'Fred Ritchin',
+    'Ixmucane': 'Ixmucané Aguilar',
+    ''
+}
