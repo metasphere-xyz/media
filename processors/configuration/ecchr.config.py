e7fd799518d8e38e886a822285eaf7cbfe06e0ff
Julian Fleck <mail@julianfleck.net>
2021-03-30 00:13:40 +0200

    added ecchr.config.py

--- processors/configuration/ecchr.config.py
+++ processors/configuration/ecchr.config.py
@@ -7,18 +7,32 @@ episodes = {
         'Visuals of Violence with Mark Sealy and Wolfgang Kaleck',
     '7cd79ca6d4999a07e969b647a82057eb':
         'On artistic and human rights interventions 1/4 with Fred Ritchin and Wolfgang Kaleck',
+    '2792e8ea6aa32398937276841d9b2414':
+        'On Artistic and Human Rights Interventions 2/4 with Susan Meiselas and Wolfgang Kaleck',
+    'f8487321ec78207ffa6a25f9fbff7079':
+        'On Artistic and Human Rights Interventions 3/4 with Rabih Mroué and Wolfgang Kaleck',
+    '102d3a312808a19439b8a7b6f9917dbe':
+        'On Artistic and Human Rights Interventions 4/4 with Christina Varvia and Wolfgang Kaleck',
     '275b1e9261bbfb863c0e62cfa2aa9d73':
         'Visuals of Violence with Mark Sealy and Wolfgang Kaleck',
     '445b28f26e7c2d54a6cbc6deeb57bc49':
-        'Ixmucané Aguilar and Wolfgang Kaleck'
+        'Ixmucané Aguilar and Wolfgang Kaleck',
+    '455f1001b9fcfda09b2b471e941853e4':
+        'The Blood of the Dawn with Claudia Salazar Jimenéz and Karina Theurer'
 }
 
 # Entity matching
 
 matched_entities = {
-    'Mark': 'Mark Sealy',
     'Wolfgang': 'Wolfgang Kaleck',
     'Fred': 'Fred Ritchin',
+    'Susan': 'Susan Meiselas',
+    'Rabih': 'Rabih Mroué',
+    'Mark': 'Mark Sealy',
     'Ixmucane': 'Ixmucané Aguilar',
-    ''
+    'Christina': 'Christina Varvia',
+    'Ixmucane': 'Ixmucané Aguilar',
+    'Claudia': 'Claudia Salazar Jimenéz',
+    'Karina': 'Karina Theurer',
+    '': ''
 }
