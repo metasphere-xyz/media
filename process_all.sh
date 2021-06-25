78c4a827229c375b7b1a5d6c39fdc5e59994d46c
asturias-leyenda <schneidermalte49@gmail.com>
2021-06-25 11:51:10 +0200

    bug fixing

--- process_all.sh
+++ process_all.sh
@@ -3,6 +3,24 @@
 # source env/bin/activate
 # pyenv activate venv-3.8.6
 
+
+# Priya & Wolfgang
+
+# ./processors/pushCollection.py --task store_collection files/podcasts/episodes/46e3411e23979c876587f3e3150399bc/full_episode.json -vv
+
+# ./processors/pushCollection.py --task extract_entities --task store_entities files/podcasts/episodes/46e3411e23979c876587f3e3150399bc/full_episode.json -vv
+
+
+# Eyal & Wolfgang
+
+# ./processors/pushCollection.py --task store_collection files/podcasts/episodes/44cd9536e0fbf498b12797921e789656/full_episode.json -vv
+
+./processors/pushCollection.py --task extract_entities --task store_entities files/podcasts/episodes/44cd9536e0fbf498b12797921e789656/full_episode.json -vv
+
+
+
+
+
 # Wolfgang & Christina
 # ./processors/pushCollection.py --task store_collection files/podcasts/episodes/102d3a312808a19439b8a7b6f9917dbe/full_episode.json -vv
 
@@ -10,7 +28,9 @@
 
 # ./processors/pushCollection.py --task extract_entities --task store_entities files/podcasts/episodes/102d3a312808a19439b8a7b6f9917dbe/full_episode.json -vv
 
-./processors/pushCollection.py --task find_similar_chunks files/podcasts/episodes/102d3a312808a19439b8a7b6f9917dbe/full_episode.json -vv
+# ./processors/pushCollection.py --task store_collection files/podcasts/episodes/66bcd542e20cac10a5dd7e6b23c27c5b/full_episode.json -vv
+
+# ./processors/pushCollection.py --task extract_entities --task store_entities files/podcasts/episodes/66bcd542e20cac10a5dd7e6b23c27c5b/full_episode.json -vv
 
 # ./processors/pushCollection.py --task extract_entities --task store_entities files/podcasts/episodes/102d3a312808a19439b8a7b6f9917dbe/full_episode.json
 
